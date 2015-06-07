0a4cb31143d194cbf8d5d3aa3567c9f4ed0b05da
Adam Brady <adam@boxxen.org>
2015-06-07 20:18:51 +1000

    change ConsoleDisplay to es6 class, inherits from Display

--- displays/console.js
+++ displays/console.js
@@ -1,47 +1,47 @@
-const EMPTY_MEM_FUNC = function () { return (new Array(64*32+1)).join(0).split('').map(function(num) { return parseInt(num, 10); }); };
 
-let ConsoleDisplay = function() {
+import Display from "./display";
 
-  this.memory = EMPTY_MEM_FUNC();
+class ConsoleDisplay extends Display {
 
-};
-
-ConsoleDisplay.prototype._draw = function() {
-
-  let n = -1;
-  while(++n < 32) {
-    let slice = this.memory.slice(n*64, n*64+64);
-    this._log(slice.join(''));
+  constructor() {
+    super();
+    this._log = console.log;
   }
 
-};
-
-ConsoleDisplay.prototype.clear = function() {
-
-  this.memory = EMPTY_MEM_FUNC();
-  this._draw();
-
-};
-
-ConsoleDisplay.prototype.draw = function(x, y, sprite) {
-  let redraw = false;
-
-  for(let n = 0; n < sprite.length; n++) {
-    for (let spriteBit = 0; spriteBit < 8; spriteBit++) {
-      let nx = (x + spriteBit) % 64;
-      let ny = (y + n) % 32;
-      let memOffset = 64*(ny) + nx;
-      let spriteValue = (sprite[n] & Math.pow(2, 7-spriteBit)) >> 7-spriteBit;
-      redraw = redraw || (this.memory[memOffset] === 1 && spriteValue === 1);
-      this.memory[memOffset] ^= spriteValue;
+  _draw() {
+    let n = -1;
+    while(++n < 32) {
+      let slice = this.memory.slice(n*64, n*64+64);
+      this._log(slice.join(''));
     }
   }
 
-  this._draw();
+  clear() {
+    super.clear();
+    this._draw();
+  }
 
-  return redraw ? 1 : 0;
+  draw(x, y, sprite) {
+
+    let redraw = false;
+
+    for(let n = 0; n < sprite.length; n++) {
+      for (let spriteBit = 0; spriteBit < 8; spriteBit++) {
+        let nx = (x + spriteBit) % 64;
+        let ny = (y + n) % 32;
+        let memOffset = 64*(ny) + nx;
+        let spriteValue = (sprite[n] & Math.pow(2, 7-spriteBit)) >> 7-spriteBit;
+        redraw = redraw || (this.memory[memOffset] === 1 && spriteValue === 1);
+        this.memory[memOffset] ^= spriteValue;
+      }
+    }
+
+    this._draw();
+
+    return redraw ? 1 : 0;
+
+  }
+
 };
-
-ConsoleDisplay.prototype._log = console.log;
 
 export default ConsoleDisplay;
